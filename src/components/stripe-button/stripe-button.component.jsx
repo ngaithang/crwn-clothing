537a93d2468e44a81b30df4c1cf30fcc63da8ad8
sean-choo <sean.choo@ada-asia.com>
2020-10-27 15:58:19 +0800

    updating stripe integration, updating title and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HgmSKAEVP9cvyJte6e6U8c0JuZKQY5zbWkfDAupaAouKEvzOaWOiheUdJL2L0t4c3xDhkHvaYIBishOSDbdkEAZ00bJXzRUTb";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="3Points Ltd"
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
